435cd4dc2238e1678f2cef73d33ff558315fc591
parkerjacks <60246140+parkerjacks@users.noreply.github.com>
2020-01-29 19:18:39 -0500

    Add files via upload
    created game logic

---
+++ memory-gameGA/js/main.js
@@ -0,0 +1,17 @@
+console.log("Up and running!"); 
+
+
+let cardOne = "queen";
+
+let cardTwo = "queen";
+
+let cardThree = "king";
+
+let cardFour = "king"; 
+
+console.log(cardFour); 
+console.log(cardTwo);
+
+console.log("User flipped " + cardFour); 
+
+
