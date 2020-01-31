975c9b75fc93bc6127b3a58d6bd8070b39283efe
parkerjacks <60246140+parkerjacks@users.noreply.github.com>
2020-01-31 02:05:28 -0500

    Add files via upload
    added image flip

--- memory-gameGA/js/main.js
+++ memory-gameGA/js/main.js
@@ -1,17 +1,84 @@
-console.log("Up and running!"); 
 
 
-let cardOne = "queen";
 
-let cardTwo = "queen";
 
-let cardThree = "king";
+const cards =  
 
-let cardFour = "king"; 
 
-console.log(cardFour); 
-console.log(cardTwo);
+[ 
 
-console.log("User flipped " + cardFour); 
+{ 
+	rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
 
+},
+{ 
+	rank: "queen", 
+suit: "diamonds", 
+cardImage: "images/queen-of-diamonds.png"
+
+}, 
+{ 
+	rank: "king", 
+suit: "hearts", 
+cardImage: "images/king-of-hearts.png"
+
+}, 
+{ 
+	rank: "king",
+suit: "diamonds", 
+cardImage: "images/king-of-diamonds.png"
+
+}
+
+
+];
+
+const cardsInPlay = []; 
+
+function checkForMatch(){if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
+
+;}
+
+function flipCard (cardId) {
+	console.log("User flipped " + cards[cardId].rank);
+
+
+cardsInPlay.push(cards[cardId].rank);  
+if (cardsInPlay.length === 2) {checkForMatch(); 
+} console.log(cards[cardId].cardImage + cards[cardId].suit);
+};  
+
+
+
+
+
+flipCard(0); 
+flipCard(1); 
+
+
+	
+
+
+
+
+
+
+	
+
+
+
+
+
+	 
+
+
+
+
+ 
 
